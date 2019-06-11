5f1e06c89330d200c3acaa2dfa6ac5373a78940c
Ivaylo Sakaliev <ivo.sakaliev1996@gmail.com>
2019-06-11 21:53:25 +0300

    add route login

--- StudentSystem_ASP_2016-master/StudentSystem2016/App_Start/RouteConfig.cs
+++ StudentSystem_ASP_2016-master/StudentSystem2016/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace StudentSystem2016
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "Login",
+               url: "Login",
+               defaults: new { controller = "Login", action = "Index" };
+
+           routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
